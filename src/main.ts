94337ef0d31d967160ac5b25f15cdbb2d23df663
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-04-25 16:12:35 +0100

    [BOT] Actually download the torrent

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import discord = require('discord.js');
 require('discord-reply');
+import webTorrent = require('webtorrent')
+import CryptoJS = require('crypto-js');
+
 const client = new discord.Client();
+const webtorrent_client = new webTorrent();
+
+const local_out_dir = "/srv/http/";
+const remote_out_dir = "http://server_address/";
 
 client.on('ready', () => {
     if (client.user === null) {
@@ -10,8 +17,8 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}. ID ${client.user.id}`);
 });
 
-function split_line(line : string) : string[] | undefined {
-    var res : string[] | undefined = new Array();
+function split_line(line: string): string[] | undefined {
+    var res: string[] | undefined = new Array();
 
     res = line.replace(/\s+/g, ' ').trim().split(' ');
 
@@ -22,8 +29,8 @@ function split_line(line : string) : string[] | undefined {
     return res;
 }
 
-function split_message(message : string) : string[][] | undefined {
-    var res : string[][] = new Array();
+function split_message(message: string): string[][] | undefined {
+    var res: string[][] = new Array();
     var lines = message.split('\n');
 
     lines.forEach(line => {
@@ -36,7 +43,7 @@ function split_message(message : string) : string[][] | undefined {
     return res.length === 0 ? undefined : res;
 }
 
-function getRole(guild : discord.Guild | null) : string {
+function getRole(guild: discord.Guild | null): string {
     if (guild === null) {
         return "error: no guild";
     }
@@ -47,9 +54,57 @@ function getRole(guild : discord.Guild | null) : string {
     return myRole.id;
 }
 
-function reply_to_message(message : discord.Message, response : string) {
-    const fake_message : any = message;
-    fake_message.lineReply("Invalid empty message.");
+function reply_to_message(message: discord.Message, response: string) {
+    const fake_message: any = message;
+    fake_message.lineReply(response);
+}
+
+function handle_line(message: discord.Message, line: string[]) {
+    var url = line[0];
+
+    console.log(`Downloading ${url}`);
+
+    const hash = CryptoJS.SHA256(url);
+    const torrent = webtorrent_client.add(url,
+        { path: `${local_out_dir}/${hash}` });
+    torrent.on('done', function () {
+        let file_str : string[] = new Array();
+        torrent.files.forEach((file) => {
+            file_str.push(`${remote_out_dir}/${hash}/${file.path}`);
+        });
+        reply_to_message(message, file_str.join('\n').replace(/ /g, "%20"));
+        torrent.destroy();
+    });
+
+    var emoji = client.emojis.cache.find(emoji => emoji.name === "torrent_ack");
+
+    let promise: Promise<void | discord.MessageReaction> = Promise.resolve();
+
+    promise = promise.then(() => {
+        if (emoji !== undefined) {
+            message.react(emoji);
+        }
+    });
+    let previous_progress = 101;
+    torrent.on('download', function (bytes) {
+        let cur_progress = ((100 * (torrent.progress + 0.001) / 10) | 0) * 10;
+        if (cur_progress != previous_progress && promise !== undefined) {
+            const new_emoji_name = "torrent_" + cur_progress;
+            const old_emoji_name = "torrent_" + previous_progress;
+            promise = promise.then(async () => {
+                const old_emoji = message.reactions.cache.find(r => r.emoji.name == old_emoji_name);
+                if (old_emoji) {
+                    await old_emoji.users.remove(client.user?.id);
+                }
+                var emoji = client.emojis.cache.find(emoji => emoji.name === new_emoji_name);
+                if (emoji !== undefined) {
+                    var real_emoji = emoji;
+                    await message.react(real_emoji);
+                }
+            });
+            previous_progress = cur_progress;
+        }
+    });
 }
 
 client.on('message', message => {
@@ -59,10 +114,10 @@ client.on('message', message => {
     }
 
     const role_id = getRole(message.guild);
-    const tag : RegExp = new RegExp(`<@(!${client.user.id}|&${role_id})>`);
+    const tag: RegExp = new RegExp(`<@(!${client.user.id}|&${role_id})>`);
     let text = message.content;
     console.log(text);
-	if (!text.match(tag)) {
+    if (!text.match(tag)) {
         return;
     }
     text = text.replace(tag, "");
@@ -74,6 +129,10 @@ client.on('message', message => {
         console.error("Received an empty message");
         return;
     }
+
+    splitted.forEach(line => {
+        handle_line(message, line);
+    });
 });
 
 client.login("INSERT YOUR TOKEN HERE");
