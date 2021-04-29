33dd42854d6c42198e7b6caf750039cff03dd391
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-04-30 00:22:00 +0100

    [COMMAND][REMIND] Add serializer/parser

--- src/reminder.ts
+++ src/reminder.ts
@@ -1,4 +1,6 @@
 import discord = require('discord.js');
+import { Result } from 'typescript-result';
+import { get_discord_manager } from './discord_manager';
 
 function reply_to_message(message: discord.Message, response: string) {
     const fake_message: any = message;
@@ -12,11 +14,37 @@ export class RemindState {
         this.mentions = mentions;
         this.message = message;
     }
+
     private date: number;
     private mentions: string;
     private sent: boolean = false;
     private message: discord.Message;
     private result_message: string | undefined = undefined;
+
+    serialize() {
+        var obj = {};
+        obj["date"] = this.date;
+        obj["mentions"] = this.mentions;
+        obj["sent"] = this.sent;
+        obj["guild"] = this.message.guild?.id;
+        obj["channel"] = this.message.channel.id;
+        obj["message"] = this.message.id;
+        if (this.result_message !== undefined) {
+            obj["result_message"] = this.result_message
+        }
+        return obj;
+    }
+
+    static async parse(val: any): Promise<Result<string, RemindState>> {
+        const message = await get_discord_manager().find_message(val["guild"], val["channel"], val["message"]);
+        if (message.isFailure()) {
+            return message.forward();
+        }
+        var state = new RemindState(val["date"], val["mentions"], message.value);
+        state.sent = val["sent"];
+        state.result_message = val["result_message"];
+        return Result.ok(state);
+    }
 
     set_result_url(url: string) {
         this.result_message = url;
